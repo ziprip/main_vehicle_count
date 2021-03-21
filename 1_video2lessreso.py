b22252efd644a2b39d9c3989fc1bae10143e0c9d
BigSur <45094743+ziprip@users.noreply.github.com>
2021-03-21 12:10:51 +0100

    Update 1_video2lessreso.py

--- 1_video2lessreso.py
+++ 1_video2lessreso.py
@@ -5,12 +5,12 @@ import numpy as np
 cap = cv2.VideoCapture('videos/your_video_here.mp4')
  
 fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('fullvideosmaller.mp4',fourcc, 5, (256,144))
+out = cv2.VideoWriter('name_of_output_video.mp4',fourcc, 5, (256,144)) #change the resolution of the output video
  
 while True:
     ret, frame = cap.read()
     if ret == True:
-        b = cv2.resize(frame,(256,144),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+        b = cv2.resize(frame,(256,144),fx=0,fy=0, interpolation = cv2.INTER_CUBIC) #change the resolution of the original video
         out.write(b)
     else:
         break
